21e18342327948590d3f503c43860cb5b84ce2a7
Sean O'Beirne <sean.t.obeirne@gmail.com>
2025-10-27 13:24:19 -0400

    cleaning

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -3,7 +3,7 @@ package models
 type Todo struct {
 	ID          int    `json:"id"`
 	Description string `json:"description"`
-	Priority    string `json:"priority"`
+	Priority    int    `json:"priority"`
 	Deleted     bool   `json:"deleted"`
 	ProjectID   *int   `json:"project_id"`
 }
